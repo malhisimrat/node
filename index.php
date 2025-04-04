996d836c19bc52d3e29344f4234b2c8cdbd6f0c3
Simratpal singh <45265328+malhisimrat@users.noreply.github.com>
2025-04-04 16:37:54 +0530

    update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ include('header.php');
                     <div class="row">
                         <div class="col-xl-7 col-lg-9 col-md-8 col-sm-9">
                             <div class="hero__caption">
-                                <span>Committed to success</span>
+                                <span>Doc's World</span>
                                 <h1 class="cd-headline letters scale">We care about your 
                                     <strong class="cd-words-wrapper">
                                         <b class="is-visible">health</b>
