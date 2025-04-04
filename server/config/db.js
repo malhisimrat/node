af78a864f8cb9b98eea047a02b9da8972a75e9f9
Simratpal singh <45265328+malhisimrat@users.noreply.github.com>
2025-04-04 15:52:26 +0530

    backend to frontend

--- server/config/db.js
+++
@@ -1,8 +0,0 @@
-let mongoose = require('mongoose')
-const dbPath = 'mongodb+srv://malhi:Rocky13june@malhi.6rips.mongodb.net/bicycleDb'
-const options = { useNewUrlParser: true, useUnifiedTopology: true }
-mongoose.connect(dbPath, options).then(res => {
-    console.log("Db Connected")
-}).catch(err => {
-    console.log("Db Connect Err", err)
-})
